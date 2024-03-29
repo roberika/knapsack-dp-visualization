efb6683762476b6b9fedee76330f0be2ae4c0533
Robert Antonius <robert.antonius23@gmail.com>
2024-03-29 21:23:40 +0700

    Project initialization and creating the table

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [capacity, setCapacity] = useState(0)
+  const [numberOfItems, setNumberOfItems] = useState(0)
+
+  function handleKnapsackParameters(e) {
+    e.preventDefault();
+
+    const form = e.target;
+    const formData = new FormData(form);
+
+    const formJson = Object.fromEntries(formData.entries());
+    console.log(formJson);
+    setCapacity(formJson.capacity);
+    setNumberOfItems(formJson.numberOfItems)
+  }
+
+  return (
+    <>
+      <form method='post' onSubmit={handleKnapsackParameters}>
+        <table>
+          <thead>
+            <tr>
+              <th>Max Weight</th>
+              <th>Number of Items</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td><input name='capacity'></input></td>
+              <td><input name='numberOfItems'></input></td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colSpan={2}>
+                <div className='flex flex-row-reverse'>
+                  <button type='submit'>Create Table</button>
+                </div>
+              </td>
+            </tr>
+          </tfoot>
+        </table>
+      </form>
+
+      <br/>
+      <table>
+        <thead>
+          <tr>
+            <th>Value</th>
+            <th>Weight</th>
+          </tr>
+        </thead>
+          <tbody>
+            {Array.from({ length: numberOfItems }).map((empty, index) => 
+              <tr>
+                <td><input placeholder={'Value of item ' + (index+1)}></input></td>
+                <td><input placeholder={'Weight of item ' + (index+1)}></input></td>
+              </tr>
+            )}
+          </tbody>
+      </table>
+    </>
+  )
+}
+
+export default App
